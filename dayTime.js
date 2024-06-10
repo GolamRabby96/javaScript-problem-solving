4efd63f2a577df1932be588a4ee63bafae51a6ab
Md Rabby Hasan <golamrabbynwu@gmail.com>
2024-06-10 06:48:28 +0600

    Daytime Print

---
+++ dayTime.js
@@ -0,0 +1,36 @@
+const today = new Date();
+const day = today.getDay();
+const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+console.log("Today is: " + dayList[day] + '.');
+
+const hour = today.getHours();
+const minute = today.getMinutes();
+const second = today.getSeconds();
+
+let prepand = (hour >= 12) ? "PM" : "AM";
+let hourModify = (hour >= 12) ? hour - 12 : hour;
+
+// Check for special cases when hour is 0 PM
+if (hourModify === 0 && prepand === 'PM') {
+    if (minute === 0 && second === 0) {
+        hourModify = 12;
+        prepand = 'Noon';
+    } else {
+        hour = 12;
+        prepand = 'PM'
+    }
+}
+
+// Same case for AM
+if (hourModify === 0 && prepand === 'AM') {
+    if (minute === 0 && second === 0) {
+        hourModify = 12;
+        prepand = 'Midnight'
+    } else {
+        hourModify = 12;
+        prepand = 'AM'
+    }
+}
+
+console.log('Current Time: ' + hourModify + prepand + ':' + minute + ':' + second);
